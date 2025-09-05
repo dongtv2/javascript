d1da0b35ba14589343c18ec51e9918280f12ad18
DongThan <thanvandong@gmail.com>
2025-09-05 22:34:02 +0700

    New commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Viết 1 function tính tổng của 2 số
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(1, 2));
+
+// Viết 1 function tính tổng của 2 số
+function sum(a, b) {
+    return a + b;
+}
